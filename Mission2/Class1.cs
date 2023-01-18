2e684d4b7765f6570766d13e3e4b1556828ca4d0
DsrKrasniii <donredd3rd@gmail.com>
2023-01-17 20:19:19 -0700

    Add project files.

---
+++ Mission2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    public class PrintIt
+    {
+        private string language = "";
+        public PrintIt(string temp)
+        {
+            language = temp;
+        }
+        public void PrintName(string name)
+        {
+            if (language == "EN")
+            {
+                Console.WriteLine("Hello " + name);
+            }
+            else
+            {
+                Console.WriteLine("Privet " + name);
+            }
+        }
+    }
+}
---
+++ Mission2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mission2
+{
+    public class Program
+    {
+        //Start of program
+      //  public /*accessible outside of program*/static /*this applies to this class itself */ void /* return type (can be string, inte etc) */ Main /*name of method*/(string[] /*what the method accepts*/ args/*name of the variable the method accepts*/)
+        public static void Main (string[] args)
+        {
+            PrintIt ps;
+            string ?userName = "";
+            string language = "";
+
+            Console.WriteLine("What language do you speak: ");
+            language = Console.ReadLine();
+            Console.WriteLine("Please enter your name: ");
+            userName = Console.ReadLine();
+
+            ps = new PrintIt(language);
+
+            ps.PrintName(userName);
+        }
+        
+    }
+}
